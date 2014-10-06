72e8c86d3e856fd0da1324090da428e60584b8e3
Andrew Kennedy <akennedy2292@gmail.com>
2014-10-06 13:03:40 -0700

    replace check for _isShown with _isRendered

--- main.js
+++ main.js
@@ -25,26 +25,27 @@ var Container = Marionette.ItemView.extend({
     _.each(options.views, this.addChildView, this);
   },
 
+  render: function(){
+    this._isRendered = true;
+    Marionette.ItemView.prototype.render.apply(this, arguments);
+  },
+
   addChildView: function(view){
     this.children.add(view);
 
-    // `this._isShown` gets set when the `show` event is triggered on the view.
-    // This is setup via marionette.domRefresh[1], which gets called inside the
-    // constructor of marionette.view[2].
-    // 1. https://github.com/marionettejs/backbone.marionette/blob/0c6447179e7bd1243061a6cbe7f59b791c7f2028/src/marionette.domRefresh.js#L12 
-    // 2. https://github.com/marionettejs/backbone.marionette/blob/0c6447179e7bd1243061a6cbe7f59b791c7f2028/src/marionette.view.js#L21
-    if( this._isShown ){
+    if( this._isRendered ){
       this._renderChildView(view, this.$(this.childContainer));
     }
   },
 
   removeChildView: function(view){
     this.children.remove(view);
-    this.render();
+    this.triggerMethod('render');
   },
 
   _renderChildren : function(){
-    // check if there's a container element. if there is, use it. if not use the parent
+    // check if there's a container element. if there is, use it. if not use the
+    // parent.
     var $el = this.$el;
     if( this.childContainer ){
       $el = this.$(this.childContainer);
@@ -87,9 +88,8 @@ var view3 = new View({name: 'Cherry'});
 // initialize container with two child views
 var container = new Container({ views: [view1, view2]});
 
-// render and show container
+// render container
 container.render();
-container.trigger('show');
 
 // example of adding a child view
 // since `.trigger('show')` was called, view3 should be rendered immediately
